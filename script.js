a20d3e5bf21887d6e2903532f6cb36d6e2f489ad
Guilherme de Araujo <guiam2909@gmail.com>
2025-03-27 08:58:39 -0300

    Atividade3

---
+++ script.js
@@ -0,0 +1,6 @@
+var numero1 = prompt("Digite o primeiro numero: ");
+var numero2 = prompt("Digite o segundo numero: ");
+var somaNumeros = parseInt(numero1) + parseInt(numero2);
+var produto = parseInt(numero1) * parseInt(numero2);
+
+alert("A soma dos seus numeros é " + somaNumeros + " e a soma do produto é " + produto);
