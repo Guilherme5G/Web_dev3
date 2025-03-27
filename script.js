c233b0708ccdeb90558768815b9adc720ee289f0
Guilherme de Araujo <guiam2909@gmail.com>
2025-03-27 09:29:55 -0300

    Exercicio sobre algoritmo JS

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-var numero1 = prompt("Digite o primeiro numero: ");
-var numero2 = prompt("Digite o segundo numero: ");
-var somaNumeros = parseInt(numero1) + parseInt(numero2);
-var produto = parseInt(numero1) * parseInt(numero2);
+//var numero1 = prompt("Digite o primeiro numero: ");
+//var numero2 = prompt("Digite o segundo numero: ");
+//var somaNumeros = parseInt(numero1) + parseInt(numero2);
+//var produto = parseInt(numero1) * parseInt(numero2);
 
-alert("A soma dos seus numeros é " + somaNumeros + " e a soma do produto é " + produto);
+//alert("A soma dos seus numeros é " + somaNumeros + " e a soma do produto é " + produto);
+
+var nome = prompt("Me fale seu nome: ");
+var n1 = prompt("Me fale um numero: ");
+var n2 = prompt("Me fale outro numero: ");
+var soma = parseInt(n1) + parseInt(n2);
+
+alert(`Ola ${nome}, o ${n1} somado á ${n2} resulta em ${soma}`)
+
+
+
+
+
